198522c35503b40c4f2919ff03c360ebef239483
MadihaJawad <madihanaveed306@gmail.com>
2023-09-11 22:44:19 +0500

    done

---
+++ src/components/BAInput.tsx
@@ -0,0 +1,13 @@
+import TextField from  '@mui/material/TextField';
+
+
+export default function BAInput (){
+    return 
+    <>
+    <TextField label='test' variant='outlined' />
+    
+    
+    
+    
+    </>
+}
